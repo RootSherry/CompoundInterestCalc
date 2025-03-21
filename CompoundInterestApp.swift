55e7006e4876998d384e86f1c8f6aeee09aadd3c
Liu Kai <121684306+RootSherry@users.noreply.github.com>
2025-03-21 23:03:16 +0800

    添加App主入口文件

---
+++ CompoundInterestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompoundInterestApp.swift
+//  复利计算器
+//
+//  Created by ROOT. on 2025/3/20.
+//
+
+import SwiftUI
+
+@main
+struct CompoundInterestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
